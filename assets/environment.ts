5e5ee5f8111fda619e47c0a0b37e798ca8254b52
Lina Beltrán <lmbd92@hotmail.com>
2021-09-10 20:40:36 -0500

    adding main and environment files

---
+++ assets/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  API_KEY: "89af8964112ea3350775c024cda415b8",
+  BASE_URL: "https://gateway.marvel.com/v1/public",
+};
---
+++ main.ts
@@ -0,0 +1,4 @@
+import { environment } from "./assets/environment";
+
+const $ = (query) => document.querySelector(query)
+const $$ = (query) => document.querySelectorAll(query)
